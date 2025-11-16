11fd3e7432b16cc4000858a18ee0d93e7f166ba0
Sudharshan R <sudharshanrms2004@gmail.com>
2025-11-16 15:21:10 +0530

    Added exam form validation

---
+++ src/containers/faculty/exams/new/FormCard.tsx
@@ -0,0 +1,441 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { Label } from "@/components/ui/label";
+import {
+  MultiSelect,
+  MultiSelectContent,
+  MultiSelectGroup,
+  MultiSelectItem,
+  MultiSelectTrigger,
+  MultiSelectValue,
+} from "@/components/ui/multi-select";
+import { Textarea } from "@/components/ui/textarea";
+import { create_exam } from "@/lib/server_api/faculty";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Percent } from "lucide-react";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const examSchema = z.object({
+  name: z.string().min(1, "Exam title is required"),
+  // department_id: z.number(),
+  // TODO: Add descrption to migration
+  description: z.string().min(1, "Description is required"),
+  exam_types: z.enum(["verbal", "logical", "qt"]).array().optional(),
+  duration_in_minutes: z.coerce
+    .number()
+    .min(1, "Duration must be at least 1 minute"),
+  start_time: z.string().refine((val) => !isNaN(Date.parse(val)), {
+    message: "Invalid start time",
+  }),
+  end_time: z.string().refine((val) => !isNaN(Date.parse(val)), {
+    message: "Invalid end time",
+  }),
+  max_violation: z.coerce.number().min(1, "Max violation must be at least 1"),
+  passing_percentage: z.coerce.number().min(0).max(100),
+  instructions: z.string().optional(),
+  show_answers: z.boolean().default(false),
+  // TODO: Add proctoring to migration
+  add_proctoring: z.boolean().default(true),
+  // TODO: Add max_attempt field
+});
+
+export default function FormCard() {
+  const [examTypeValues, setExamTypeValues] = useState<string[]>([]);
+  const form = useForm({
+    resolver: zodResolver(examSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof examSchema>) {
+    console.log(data);
+    data.department_id = 1;
+    if (examTypeValues.length == 0) {
+      console.log("Exam Types: ", examTypeValues);
+      form.setError("exam_types", {
+        message: "Please select at least one exam type",
+      });
+      return;
+    }
+
+    const response = await create_exam(data);
+    if (response.status) {
+      toast.success("Exam created successfully");
+      form.reset();
+    } else {
+      toast.error(response.message || "Unable to create exam");
+    }
+  }
+
+  return (
+    <>
+      <form
+        action=""
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="container my-8 space-y-6"
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle>Basic Information</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <FieldGroup>
+              <Controller
+                control={form.control}
+                name="name"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      htmlFor="name"
+                      aria-invalid={fieldState.invalid}
+                    >
+                      Exam Title *
+                    </FieldLabel>
+                    <Input
+                      id="name"
+                      placeholder="Enter exam title"
+                      {...field}
+                      aria-invalid={fieldState.invalid}
+                    />
+                    {fieldState.error && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                control={form.control}
+                name="description"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      htmlFor="description"
+                      aria-invalid={fieldState.invalid}
+                    >
+                      Description *
+                    </FieldLabel>
+                    <Textarea
+                      id="description"
+                      placeholder="Brief description of the exam"
+                      rows={3}
+                      {...field}
+                      aria-invalid={fieldState.invalid}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                control={form.control}
+                name="exam_types"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      htmlFor="exam_types"
+                      aria-invalid={fieldState.invalid}
+                    >
+                      Exam Type Mapping *
+                    </FieldLabel>
+                    <MultiSelect
+                      {...field}
+                      onValuesChange={(values) => {
+                        setExamTypeValues(values);
+                        field.onChange(values);
+                      }}
+                    >
+                      <MultiSelectTrigger className="w-full">
+                        <MultiSelectValue placeholder="Search Exam Type..." />
+                      </MultiSelectTrigger>
+                      <MultiSelectContent>
+                        <MultiSelectGroup>
+                          <MultiSelectItem value="verbal">
+                            Vebal
+                          </MultiSelectItem>
+                          <MultiSelectItem value="logical">
+                            Logical
+                          </MultiSelectItem>
+                          <MultiSelectItem value="qt">
+                            Quantative
+                          </MultiSelectItem>
+                        </MultiSelectGroup>
+                      </MultiSelectContent>
+                    </MultiSelect>
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Exam Configuration</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <FieldGroup className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <Controller
+                control={form.control}
+                name="duration_in_minutes"
+                render={({ field, fieldState }) => (
+                  <Field
+                    data-invalid={fieldState.invalid}
+                    className="col-span-2"
+                  >
+                    <FieldLabel
+                      aria-invalid={fieldState.invalid}
+                      htmlFor="duration_in_minutes"
+                    >
+                      Duration (minutes) *
+                    </FieldLabel>
+                    <Input
+                      id="duration_in_minutes"
+                      type="number"
+                      min="1"
+                      {...field}
+                      aria-invalid={fieldState.invalid}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                control={form.control}
+                name="start_time"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      aria-invalid={fieldState.invalid}
+                      htmlFor="start_time"
+                    >
+                      Start Time *
+                    </FieldLabel>
+                    <Input
+                      id="start_time"
+                      aria-invalid={fieldState.invalid}
+                      type="datetime-local"
+                      {...field}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                control={form.control}
+                name="end_time"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      aria-invalid={fieldState.invalid}
+                      htmlFor="end_time"
+                    >
+                      End Time *
+                    </FieldLabel>
+                    <Input
+                      aria-invalid={fieldState.invalid}
+                      id="end_time"
+                      type="datetime-local"
+                      {...field}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                control={form.control}
+                name="max_violation"
+                render={({ field, fieldState }) => (
+                  <Field
+                    data-invalid={fieldState.invalid}
+                    className="col-span-2"
+                  >
+                    <FieldLabel
+                      aria-invalid={fieldState.invalid}
+                      htmlFor="max_violation"
+                    >
+                      Max Violation *
+                    </FieldLabel>
+                    <Input
+                      aria-invalid={fieldState.invalid}
+                      id="max_violation"
+                      type="number"
+                      {...field}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                control={form.control}
+                name="passing_percentage"
+                render={({ field, fieldState }) => (
+                  <Field
+                    data-invalid={fieldState.invalid}
+                    className="col-span-2"
+                  >
+                    <FieldLabel
+                      aria-invalid={fieldState.invalid}
+                      htmlFor="passing_percentage"
+                    >
+                      Passing Percentage *
+                    </FieldLabel>
+                    <InputGroup>
+                      <InputGroupInput
+                        aria-invalid={fieldState.invalid}
+                        {...field}
+                        id="passing_percentage"
+                        type="number"
+                      />
+                      <InputGroupAddon align="inline-end">
+                        <Percent />
+                      </InputGroupAddon>
+                    </InputGroup>
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                control={form.control}
+                name="instructions"
+                render={({ field, fieldState }) => (
+                  <Field
+                    data-invalid={fieldState.invalid}
+                    className="col-span-2"
+                  >
+                    <FieldLabel
+                      htmlFor="instructions"
+                      aria-invalid={fieldState.invalid}
+                    >
+                      Instructions
+                    </FieldLabel>
+                    <Textarea
+                      id="instructions"
+                      placeholder="Exam instructions for students"
+                      rows={4}
+                      aria-invalid={fieldState.invalid}
+                      {...field}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Advanced Settings</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <FieldGroup className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <Controller
+                control={form.control}
+                name="show_answers"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      aria-invalid={fieldState.invalid}
+                      className="hover:bg-accent/50 flex items-start gap-3 rounded-lg border p-3 has-[[aria-checked=true]]:border-blue-600 has-[[aria-checked=true]]:bg-blue-50 dark:has-[[aria-checked=true]]:border-blue-900 dark:has-[[aria-checked=true]]:bg-blue-950"
+                    >
+                      <Checkbox
+                        id="toggle-2"
+                        className="data-[state=checked]:border-blue-600 data-[state=checked]:bg-blue-600 data-[state=checked]:text-white dark:data-[state=checked]:border-blue-700 dark:data-[state=checked]:bg-blue-700"
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                      <div className="grid gap-1.5 font-normal">
+                        <p className="text-sm leading-none font-medium">
+                          Show Results Immediately
+                        </p>
+                        <p className="text-muted-foreground text-sm">
+                          Display results after completion
+                        </p>
+                      </div>
+                    </FieldLabel>
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                control={form.control}
+                name="add_proctoring"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      data-invalid={fieldState.invalid}
+                      className="hover:bg-accent/50 flex items-start gap-3 rounded-lg border p-3 has-[[aria-checked=true]]:border-blue-600 has-[[aria-checked=true]]:bg-blue-50 dark:has-[[aria-checked=true]]:border-blue-900 dark:has-[[aria-checked=true]]:bg-blue-950"
+                    >
+                      <Checkbox
+                        id="toggle-2"
+                        defaultChecked
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                        className="data-[state=checked]:border-blue-600 data-[state=checked]:bg-blue-600 data-[state=checked]:text-white dark:data-[state=checked]:border-blue-700 dark:data-[state=checked]:bg-blue-700"
+                      />
+                      <div className="grid gap-1.5 font-normal">
+                        <p className="text-sm leading-none font-medium">
+                          Add Proctoring
+                        </p>
+                        <p className="text-muted-foreground text-sm">
+                          Enables proctoring for student exam
+                        </p>
+                      </div>
+                    </FieldLabel>
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </CardContent>
+        </Card>
+        <Button type="submit" className="float-right">
+          Submit
+        </Button>
+      </form>
+    </>
+  );
+}
--- src/lib/server_api/faculty.ts
+++ src/lib/server_api/faculty.ts
@@ -1,103 +1,158 @@
-'use server';
+"use server";
 
 import Request from "@/utils/Request";
 
-export async function get_exam_results({ exam_id }: { exam_id: number}){
-    try{
-        const response = await Request({
-            url: process.env.BACKEND_HOST + `/api/faculty/exams/${exam_id}/results`,
-            isAuthorized: true,
-        });
+export async function get_exam_results({ exam_id }: { exam_id: number }) {
+  try {
+    const response = await Request({
+      url: process.env.BACKEND_HOST + `/api/faculty/exams/${exam_id}/results`,
+      isAuthorized: true,
+    });
 
-        return {
-            status: true,
-            data: response.data.data
-        }
-    }catch(error: any){
-        return {
-            status: false,
-            message: error.response?.data?.message || "Unknown error occurred"
-        }
-    }
+    return {
+      status: true,
+      data: response.data.data,
+    };
+  } catch (error: any) {
+    return {
+      status: false,
+      message: error.response?.data?.message || "Unknown error occurred",
+    };
+  }
 }
 
-export async function get_all_exams(){
-    try{
-        const response = await Request({
-            url: process.env.BACKEND_HOST + `/api/faculty/exams`,
-            isAuthorized: true,
-        });
+export async function get_all_exams() {
+  try {
+    const response = await Request({
+      url: process.env.BACKEND_HOST + `/api/faculty/exams`,
+      isAuthorized: true,
+    });
 
-        return {
-            status: true,
-            data: response.data.data
-        }
-    }catch(error: any){
-        return {
-            status: false,
-            message: error.response?.data?.message || "Unable to fetch exams"
-        }
-    }
+    return {
+      status: true,
+      data: response.data.data,
+    };
+  } catch (error: any) {
+    return {
+      status: false,
+      message: error.response?.data?.message || "Unable to fetch exams",
+    };
+  }
 }
 
-export async function get_exam_violations({ exam_id }: { exam_id: number }){
-    try{
-        const response = await Request({
-            url: process.env.BACKEND_HOST + `/api/faculty/exams/${exam_id}/violations`,
-            isAuthorized: true,
-        })
-        return {
-            status: true,
-            data: response.data.data
-        }
-    }catch(error: any){
-        return {
-            status: false,
-            message: error.response?.data?.message || "Unable to fetch exam violations"
-        }
-    }
+export async function get_exam_violations({ exam_id }: { exam_id: number }) {
+  try {
+    const response = await Request({
+      url:
+        process.env.BACKEND_HOST + `/api/faculty/exams/${exam_id}/violations`,
+      isAuthorized: true,
+    });
+    return {
+      status: true,
+      data: response.data.data,
+    };
+  } catch (error: any) {
+    return {
+      status: false,
+      message:
+        error.response?.data?.message || "Unable to fetch exam violations",
+    };
+  }
 }
 
-export async function get_exam_types(){
-    try{
-        const response = await Request({
-            url: process.env.BACKEND_HOST + `/api/faculty/exam-types`,
-            isAuthorized: true,
-        });
+export async function get_exam_types() {
+  try {
+    const response = await Request({
+      url: process.env.BACKEND_HOST + `/api/faculty/exam-types`,
+      isAuthorized: true,
+    });
 
-        return {
-            status: true,
-            data: response.data.data
-        }
-    }catch(error: any){
-        return {
-            status: false,
-            message: error.response?.data?.message || "Unable to fetch exam types"
-        }
-    }
+    return {
+      status: true,
+      data: response.data.data,
+    };
+  } catch (error: any) {
+    return {
+      status: false,
+      message: error.response?.data?.message || "Unable to fetch exam types",
+    };
+  }
 }
 
-export async function create_exam_type({ name, description, is_private }: { name: string, description?: string | undefined, is_private?: boolean | undefined }){
-    try{
-        const response = await Request({
-            url: process.env.BACKEND_HOST + `/api/faculty/exam-types`,
-            method: 'POST',
-            isAuthorized: true,
-            body: {
-                name,
-                description,
-                is_private
-            }
-        });
+export async function create_exam_type({
+  name,
+  description,
+  is_private,
+}: {
+  name: string;
+  description?: string | undefined;
+  is_private?: boolean | undefined;
+}) {
+  try {
+    const response = await Request({
+      url: process.env.BACKEND_HOST + `/api/faculty/exam-types`,
+      method: "POST",
+      isAuthorized: true,
+      body: {
+        name,
+        description,
+        is_private,
+      },
+    });
 
-        return {
-            status: true,
-            data: response.data.data
-        }
-    }catch(error: any){
-        return {
-            status: false,
-            message: error.response?.data?.message || "Unable to create exam type"
-        }
-    }
+    return {
+      status: true,
+      data: response.data.data,
+    };
+  } catch (error: any) {
+    return {
+      status: false,
+      message: error.response?.data?.message || "Unable to create exam type",
+    };
+  }
+}
+
+export async function get_exam_type_questions({
+  exam_type_id,
+}: {
+  exam_type_id: number;
+}) {
+  try {
+    const response = await Request({
+      url:
+        process.env.BACKEND_HOST + `/api/exam/types/${exam_type_id}/questions`,
+    });
+
+    return {
+      status: true,
+      data: response.data.data,
+    };
+  } catch (error: any) {
+    return {
+      status: false,
+      message:
+        error.response?.data?.message || "Unable to fetch exam type questions",
+    };
+  }
+}
+
+export async function create_exam(body: object) {
+  try {
+    const response = await Request({
+      url: process.env.BACKEND_HOST + "/api/exams",
+      method: "POST",
+      isAuthorized: true,
+      body: body,
+    });
+
+    return {
+      status: true,
+      data: response.data.data,
+    };
+  } catch (error: any) {
+    return {
+      status: false,
+      message: error.response?.data?.message || "Unable to create exam",
+    };
+  }
 }
