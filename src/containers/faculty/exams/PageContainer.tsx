f4e149da9b9d898fde9402ce96c66f5b8af3676e
Sudharshan R <sudharshanrms2004@gmail.com>
2025-11-16 21:07:49 +0530

    Listed exam from api endpoint

--- src/containers/faculty/exams/PageContainer.tsx
+++ src/containers/faculty/exams/PageContainer.tsx
@@ -1,82 +1,120 @@
-import React from 'react'
-import HeroStats from './HeroStats'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import { Edit, Eye, EyeOff, Globe, Plus } from 'lucide-react'
-import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Badge } from '@/components/ui/badge'
-import { DeleteExam } from './DeleteExam'
+"use client";
+
+import React from "react";
+import HeroStats from "./HeroStats";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Edit, Eye, EyeOff, Globe, Plus } from "lucide-react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { DeleteExam } from "./DeleteExam";
+import { useQuery } from "@tanstack/react-query";
+import { get_all_exams } from "@/lib/server_api/faculty";
+import { Exam } from "@/types/exam";
+import { cn } from "@/lib/utils";
 
 function PageContainer() {
-    return (
-        <div className='my-8 space-y-4'>
-            <HeroStats />
-            <section>
-                <div className="flex justify-between items-center">
-                    <h2 className='text-xl font-semibold'>Your Exams</h2>
-                    <Button>
-                        <Link href={"/faculty/exams/new"} className='inline-flex gap-2 items-center'>
-                            <Plus />
-                            <span>Create Exam</span>
-                        </Link>
-                    </Button>
-                </div>
-                {
-                    Array(5).fill(0).map((_, index) => (
-                        <Card className='mt-4'>
-                            <CardHeader>
-                                <div className="flex gap-2 items-center">
-                                    <div className="p-2 bg-success text-success-foreground rounded">
-                                        <Globe size={20} />
-                                    </div>
-                                    <div>
-                                        <CardTitle className='mb-2'>
-                                            first_test
-                                        </CardTitle>
-                                        <CardDescription>
-                                            <Badge>Draft</Badge>
-                                        </CardDescription>
-                                    </div>
-                                </div>
-                                <CardAction>
-                                    <Button variant="default">Publish</Button>
-                                </CardAction>
-                            </CardHeader>
-                            <CardContent>
-                                <div className="flex flex-wrap gap-8 items-start text-sm">
-                                    <div>
-                                        <p className='text-muted-foreground mb-1'>Duration</p>
-                                        <p>56 Min</p>
-                                    </div>
-                                    <div>
-                                        <p className='text-muted-foreground mb-1'>Total Students</p>
-                                        <p>60</p>
-                                    </div>
-                                    <div>
-                                        <p className='text-muted-foreground mb-1'>Start Date & time</p>
-                                        <p>11/04/2023 10:00 AM</p>
-                                    </div>
-                                    <div>
-                                        <p className='text-muted-foreground mb-1'>End Date & time</p>
-                                        <p>11/04/2023 11:00 AM</p>
-                                    </div>
-                                </div>
-                                <div className='mt-6 text-sm'>
-                                    <div className="flex justify-between">
-                                        <p className='text-muted-foreground'>Created At: 11/05/2003</p>
-                                        <div className="flex gap-2 items-center">
-                                            <Button variant="outline"><Edit /></Button>
-                                            <DeleteExam />
-                                        </div>
-                                    </div>
-                                </div>
-                            </CardContent>
-                        </Card>
-                    ))
-                }
-            </section>
+  const exams = useQuery({
+    queryKey: ["faculty", "exams"],
+    queryFn: async () => {
+      const response = await get_all_exams();
+      if (response.status) {
+        return response.data;
+      } else {
+        throw new Error(response.message);
+      }
+    },
+  });
+  return (
+    <div className="my-8 space-y-4">
+      <HeroStats />
+      <section>
+        <div className="flex justify-between items-center">
+          <h2 className="text-xl font-semibold">Your Exams</h2>
+          <Button>
+            <Link
+              href={"/faculty/exams/new"}
+              className="inline-flex gap-2 items-center"
+            >
+              <Plus />
+              <span>Create Exam</span>
+            </Link>
+          </Button>
         </div>
-    )
+        {exams.data?.map((exam: Exam, index: number) => (
+          <Card className="mt-4" key={index}>
+            <CardHeader>
+              <div className="flex gap-2 items-center">
+                <div
+                  className={cn(
+                    "p-2 rounded",
+                    exam.status
+                      ? "bg-success text-success-foreground"
+                      : "bg-warning text-warning-foreground"
+                  )}
+                >
+                  {exam.status ? <Eye size={15} /> : <EyeOff size={15} />}
+                </div>
+                <div>
+                  <CardTitle className="mb-2">{exam.name}</CardTitle>
+                  <CardDescription>
+                    <Badge>{exam.status ? "Published" : "Draft"}</Badge>
+                  </CardDescription>
+                </div>
+              </div>
+              <CardAction>
+                {exam.status == 0 && <Button variant="default">Publish</Button>}
+              </CardAction>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-wrap gap-8 items-start text-sm">
+                <div>
+                  <p className="text-muted-foreground mb-1">Duration</p>
+                  <p>{exam.duration_in_minutes} Min</p>
+                </div>
+                <div>
+                  <p className="text-muted-foreground mb-1">Total Students</p>
+                  <p>0</p>
+                </div>
+                <div>
+                  <p className="text-muted-foreground mb-1">
+                    Start Date & time
+                  </p>
+                  <p>{new Date(exam.start_time).toLocaleString()}</p>
+                </div>
+                <div>
+                  <p className="text-muted-foreground mb-1">End Date & time</p>
+                  <p>{new Date(exam.end_time).toLocaleString()}</p>
+                </div>
+              </div>
+              <div className="mt-6 text-sm">
+                <div className="flex justify-between">
+                  <p className="text-muted-foreground">
+                    Created At: {new Date(exam.created_at).toLocaleString()}
+                  </p>
+                  <div className="flex gap-2 items-center">
+                    {exam.status == 0 && (
+                      <Button variant="outline">
+                        <Edit />
+                      </Button>
+                    )}
+                    <DeleteExam />
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+      </section>
+    </div>
+  );
 }
 
-export default PageContainer
+export default PageContainer;
--- src/types/exam.ts
+++ src/types/exam.ts
@@ -1,28 +1,29 @@
 export type Exam = {
-    id: number,
-    department_id: number,
-    name: string,
-    start_time: string,
-    end_time: string,
-    duration_in_minutes: number,
-    passing_percentage: string,
-    max_attempts: number,
-    status: number,
-    show_answers: number,
-    max_violations_count: number
-}
+  id: number;
+  department_id: number;
+  name: string;
+  start_time: string;
+  end_time: string;
+  duration_in_minutes: number;
+  passing_percentage: string;
+  max_attempts: number;
+  status: number;
+  show_answers: number;
+  max_violations_count: number;
+  created_at: string;
+};
 
 export type Department = {
-    id: number,
-    school_id: number,
-    name: string
-}
+  id: number;
+  school_id: number;
+  name: string;
+};
 
 export type ExamType = {
-    id: number,
-    name: string,
-    description?: string,
-    is_private: boolean,
-    created_at: string,
-    created_by_faculty: number
-}
+  id: number;
+  name: string;
+  description?: string;
+  is_private: boolean;
+  created_at: string;
+  created_by_faculty: number;
+};
